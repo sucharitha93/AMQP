028cf4f6dc06af335d1e74defa9d482fe5bb2e25
sucharitha93 <sucharitha93@gmail.com>
2017-06-02 16:44:19 +0530

    Working version of AMQP basic program

---
+++ app/src/main/java/amrita/cse/amuda/amqp/MainActivity.java
@@ -0,0 +1,176 @@
+package amrita.cse.amuda.amqp;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupConnectionFactory();
+        publishToAMQP();
+        setupPubButton();
+
+        final Handler incomingMessageHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                String message = msg.getData().getString("msg");
+                TextView tv = (TextView) findViewById(R.id.textView);
+                Date now = new Date();
+                SimpleDateFormat ft = new SimpleDateFormat ("hh:mm:ss");
+                tv.append(ft.format(now) + ' ' + message + '\n');
+            }
+        };
+        subscribe(incomingMessageHandler);
+    }
+
+    void setupPubButton() {
+        Button button = (Button) findViewById(R.id.publish);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                EditText et = (EditText) findViewById(R.id.text);
+                publishMessage(et.getText().toString());
+                et.setText("");
+            }
+        });
+    }
+
+    private BlockingDeque queue = new LinkedBlockingDeque();
+    void publishMessage(String message) {
+
+        try {
+            queue.putLast(message);
+            Toast.makeText(getApplicationContext(),queue.toString(),Toast.LENGTH_LONG).show();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    ConnectionFactory factory = new ConnectionFactory();
+    private void setupConnectionFactory() {
+        String uri = "amqp://amuda:amuda2017@172.17.9.61:5672/%2f";
+        try {
+            factory.setAutomaticRecoveryEnabled(false);
+            factory.setUsername("amuda");
+            factory.setPassword("amuda2017");
+            factory.setHost("172.17.9.61");
+            factory.setPort(5672);
+            factory.setVirtualHost("amudavhost");
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+    public void publishToAMQP()
+    {
+        Log.i("","Reached publish to AMQP");
+        publishThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        Connection connection = factory.newConnection();
+                        Channel ch = connection.createChannel();
+                        ch.confirmSelect();
+                        Log.i("","Reached publish to AMQP");
+
+                        while(true) {
+
+                            String message = queue.takeFirst().toString();
+                            Log.i("",message+" is the message to be published");
+                            try{
+                                ch.basicPublish("amq.fanout","severity" , null, message.getBytes());
+                                ch.waitForConfirmsOrDie();
+                            } catch (Exception e){
+                                queue.putFirst(message);
+                                throw e;
+                            }
+                        }
+                    } catch (InterruptedException e) {
+                        break;
+                    } catch (Exception e) {
+                        Log.d("", "Connection broken: " + e.getClass().getName());
+                        try {
+                            Thread.sleep(1000); //sleep and then try again
+                        } catch (InterruptedException e1) {
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+        publishThread.start();
+    }
+    void subscribe(final Handler handler)
+    {
+        subscribeThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        Connection connection = factory.newConnection();
+                        Channel channel = connection.createChannel();
+                        channel.basicQos(1);
+                        AMQP.Queue.DeclareOk q = channel.queueDeclare();
+                        channel.queueBind(q.getQueue(), "amq.fanout", "severity");
+                        QueueingConsumer consumer = new QueueingConsumer(channel);
+                        channel.basicConsume(q.getQueue(), true, consumer);
+
+                        while (true) {
+                            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+                            String message = new String(delivery.getBody());
+                            Message msg = handler.obtainMessage();
+                            Bundle bundle = new Bundle();
+                            bundle.putString("msg", message);
+                            msg.setData(bundle);
+                            handler.sendMessage(msg);
+                        }
+                    } catch (InterruptedException e) {
+                        break;
+                    } catch (Exception e1) {
+                        Log.d("", "Connection broken: " + e1.getClass().getName());
+                        try {
+
+                            Thread.sleep(1000); //sleep and then try again
+                        } catch (InterruptedException e) {
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+        subscribeThread.start();
+    }
+    Thread subscribeThread;
+    Thread publishThread;
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        publishThread.interrupt();
+        subscribeThread.interrupt();
+    }
+}
